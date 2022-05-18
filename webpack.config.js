c576c3ea642f69f5c44a66307d4ea42e6ad970d1
Alex Contreras <alex.code29@gmail.com>
2022-05-18 17:20:01 -0500

    added webpack methods to optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path  = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // main config object
 module.exports = {
     // entry is the root of the bundle and the beginning of the dependency graph
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schdule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
 
     // where we want the output bundle form entry to be placed
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
     },
 
     // tell webpack that we are using libarie's global variables (i.e: jquery) in this case
@@ -17,9 +23,37 @@ module.exports = {
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
+        }), 
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
         })
     ],
 
     // the way webpack will run 
-    mode: 'development'
+    mode: 'development',
+
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", 'assets')
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    }
 };
